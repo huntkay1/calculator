6f0cf5de30e89cc754d7c6352decd486a0dda4f2
Kayla Hunt <huntkay1@msu.edu>
2024-01-30 18:25:53 -0500

    Create operator functions
    Created functions to calculate for each operator. Also added the
    calculator buttons to HTML for UI.

---
+++ script.js
@@ -0,0 +1,41 @@
+function add(array) {
+    return array.reduce((total, num) => total + num, 0)
+};
+
+function subtract(array) {
+    return array.reduce((total, num) => total - num)
+};
+
+function divide(array) {
+    return array.reduce((total, num) => total / num)
+};
+
+function multiply(array) {
+    return array.reduce((total, num) => total * num)
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "add":
+            console.log (add([num1, num2]));
+            break;
+        
+        case "sub":
+            console.log (subtract([num1, num2]));
+            break;
+        
+        case "div":
+            console.log (divide([num1, num2]));
+            break;
+        
+        case "mul":
+            console.log (multiply([num1, num2]));
+            break;
+
+    }
+}
+
+let num1
+let num2
+let operator
+
